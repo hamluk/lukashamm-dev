a86ebbc7a014ebf6fe6df39bde9a40733aa086f7
Lukas Hamm <lukas@lukashamm.dev>
2025-06-26 12:09:45 +0800

    add links to socials in header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -26,13 +26,21 @@ const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
       <div className="flex items-center gap-2">
         <div className="bg-gray-400 w-0.5 h-13 sm:h-21"></div>
         <div className="flex flex-col sm:flex-row gap-1 sm:gap-3">
-          <a>
+          <a
+            className="hover:cursor-pointer"
+            href="https://github.com/hamluk"
+            target="_blank"
+          >
             <FaGithub className="sm:size-6" />
           </a>
-          <a>
+          <a
+            className="hover:cursor-pointer"
+            href="https://www.linkedin.com/in/lukashamm-dev"
+            target="_blank"
+          >
             <FaLinkedin className="sm:size-6" />
           </a>
-          <a>
+          <a className="hover:cursor-pointer" href="mailto:lukas@lukashamm.dev">
             <FaEnvelope className="sm:size-6" />
           </a>
         </div>

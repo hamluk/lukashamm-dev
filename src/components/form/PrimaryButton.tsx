06b8cfb952706fe0b4bf2dacd325e6a4a9184be9
Lukas Hamm <lukas@lukashamm.dev>
2025-07-25 18:24:59 +0800

    update buttons text size to responsive design

--- src/components/form/PrimaryButton.tsx
+++ src/components/form/PrimaryButton.tsx
@@ -13,7 +13,7 @@ class PrimaryButton extends Component<Props, State> {
   render() {
     return (
       <a href="mailto:lukas@lukashamm.dev">
-        <button className="bg-olive-green font-semibold text-white hover:bg-(image:--gradient-primary-button) hover:cursor-pointer px-3 py-1.5 rounded-lg hover:">
+        <button className="bg-olive-green font-semibold text-white hover:bg-(image:--gradient-primary-button) hover:cursor-pointer text-sm sm:text-base px-3 py-1.5 rounded-lg hover:">
           {this.props.title}
         </button>
       </a>
--- src/components/form/SecondaryButton.tsx
+++ src/components/form/SecondaryButton.tsx
@@ -12,7 +12,7 @@ class SecondaryButton extends Component<Props, State> {
 
   render() {
     return (
-      <button className="bg-white font-semibold hover:cursor-pointer px-3 py-1 rounded-lg hover:">
+      <button className="bg-white font-semibold hover:cursor-pointer text-sm sm:text-base px-3 py-1 rounded-lg hover:">
         <p className="inline-block text-transparent bg-clip-text bg-olive-green hover:bg-(image:--gradient-primary-button)">
           {this.props.title}
         </p>

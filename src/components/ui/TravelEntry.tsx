eefbf4130e1b693c3b176d94533a356212b23d44
Lukas Hamm <lukas@lukashamm.dev>
2025-07-31 18:12:31 +0800

    improve resonsive design of travel entries for mobile devices

--- src/components/ui/TravelEntry.tsx
+++ src/components/ui/TravelEntry.tsx
@@ -8,17 +8,19 @@ type Props = {
 
 function TravelEntry({ header, dates, body, subbody, image }: Props) {
   return (
-    <section className="grid grid-cols-5 grid-rows-[80px_20px_auto_auto] text-black">
-      <h2 className="max-h-30 row-span-1 col-span-3 text-2xl">{header}</h2>
-      <p className="row-span-1 col-span-3 text-xs italic">{dates}</p>
-      <p className="row-span-1 col-span-3">{body}</p>
-      {subbody !== undefined && (
-        <p className="row-span-1 col-span-3">{subbody}</p>
-      )}
+    <section className="flex flex-col lg:flex-row gap-4 text-black">
+      <div className="flex flex-col gap-1">
+        <p className="row-span-1 col-span-3 text-xs italic">{dates}</p>
+        <h2 className="max-h-30 row-span-1 col-span-3 text-2xl">{header}</h2>
+        <p className="row-span-1 col-span-3 lg:pt-7">{body}</p>
+        {subbody !== undefined && (
+          <p className="row-span-1 col-span-3">{subbody}</p>
+        )}
+      </div>
       <img
         src={image}
         alt="Travel Image Vietnam"
-        className="col-start-4 col-span-2 row-start-1 row-span-4 justify-self-end aspect-2/3 object-cover min-h-70 max-h-100 rounded-xl"
+        className="object-cover object-[25%_85%] min-h-70 max-h-100 lg:aspect-[2/3] rounded-xl "
       ></img>
     </section>
   );
--- src/components/ui/Travels.tsx
+++ src/components/ui/Travels.tsx
@@ -8,7 +8,7 @@ type Props = {};
 
 function Travels({}: Props) {
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-2 gap-5 py-10 px-5 sm:px-10 lg:px-15 xl:px-20 2xl:px-25">
+    <section className="grid grid-cols-1 xl:grid-cols-2 gap-5 py-10 px-5 sm:px-10 lg:px-15 xl:px-20 2xl:px-25">
       {travels &&
         travels.map((entry) => (
           <TravelEntry

ab4557fa96f27936b17e79288ed0eaf99d7829f0
Lukas Hamm <lukas@lukashamm.dev>
2025-08-21 15:43:24 +0800

    adapt homepage settings after hosting site on personal domain

--- src/components/ui/Achievements.tsx
+++ src/components/ui/Achievements.tsx
@@ -8,7 +8,6 @@ import ProgrammingSVG from "/assets/programming_illustration.svg";
 import AdventureSVG from "/assets/adventure_illustration.svg";
 import BeachSVG from "/assets/beach_illustration.svg";
 import RemoteSVG from "/assets/remote_illustration.svg";
-import TestImg from "/assets/ai_assistent.png";
 
 type Props = {};
 
@@ -41,10 +40,6 @@ function Achievements({}: Props) {
           ></img>
           <img
             src={AdventureSVG}
-            className="object-contain aspect-1/1 w-20 sm:w-25 lg:w-30 xl:w-40"
-          ></img>
-          <img
-            src={TestImg}
             className="object-contain aspect-1/1 w-20 sm:w-25 lg:w-30 xl:w-40"
           ></img>
         </div>

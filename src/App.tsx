0f9501bc3da67dd6d43687c575f3947a0f17abe5
Lukas Hamm <lukas@lukashamm.dev>
2025-08-21 12:49:25 +0800

    updates vite base url updates router base url

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import AppRoutes from "./router";
 function App() {
   return (
     <LanguageProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/lukashamm-dev/">
         <AppRoutes />
       </BrowserRouter>
     </LanguageProvider>

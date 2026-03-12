1ed18579d4f92273904deaa8e8de6448c519d7c0
Lukas <lukas.hamm5000@gmail.com>
2026-03-12 14:25:41 +0100

    add motion to sections

--- src/components/ui/About.tsx
+++ src/components/ui/About.tsx
@@ -1,6 +1,7 @@
 import { useLanguage } from "../../context/LanguageContext";
 import ProfilImage from "/assets/LukasHamm.jpg";
 import SectionIntro from "./SectionIntro";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -15,7 +16,13 @@ function About({}: Props) {
         className="relative bg-cover bg-center items-center justify-around flex flex-col md:flex-row text-dark-text gap-3 py-10 px-5 sm:px-7 lg:px-10 2xl:px-25"
         style={{ backgroundImage: "url('/assets/package_cover.jpeg')" }}
       >
-        <div className="flex flex-col gap-2 bg-highlight-section p-4 w-full md:w-110 lg:w-130 xl:w-170 shadow-md rounded-lg">
+        <motion.div
+          className="flex flex-col gap-2 bg-highlight-section p-4 w-full md:w-110 lg:w-130 xl:w-170 shadow-md rounded-lg"
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.6 }}
+          transition={{ duration: 0.6, ease: "easeOut" }}
+        >
           <h2 className="text-2xl text-dark-text font-semibold md:text-4xl font-serif">
             {texts.homepageTexts.About.head}
           </h2>
@@ -33,13 +40,20 @@ function About({}: Props) {
               <span>{texts.homepageTexts.About.cta}</span>
             </div>
           </div>
-        </div>
+        </motion.div>
 
-        <img
-          src={ProfilImage}
-          alt="Image"
-          className="shadow-md max-w-xs md:max-w-90 rounded-xl"
-        ></img>
+        <motion.div
+          initial={{ opacity: 0, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.6 }}
+          transition={{ delay: 0.6, duration: 0.6, ease: "easeOut" }}
+        >
+          <img
+            src={ProfilImage}
+            alt="Image"
+            className="shadow-md max-w-xs md:max-w-90 rounded-xl"
+          ></img>
+        </motion.div>
       </section>
     </>
   );
